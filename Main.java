cd9829e066b1d69adf33da41f8ff8c05a9407189
hgrranzi <vkh.mariia@gmail.com>
2021-09-12 20:40:21 +0300

    Fixed toString method in the City class

--- Main.java
+++ Main.java
@@ -17,66 +17,9 @@ public class Main {
             info = scanner.nextLine().split(";");
             cityList.add(new City(info));
         }
+        scanner.close();
         for (City city : cityList) {
             System.out.println(city);
         }
-    }
-}
-
-class City {
-    private final int id;
-    private final String name;
-    private final String region;
-    private final String district;
-    private final int population;
-    private final String foundation;
-
-    public City(String[] info) {
-        this.id = Integer.parseInt(info[0]);
-        this.name = info[1];
-        this.region = info[2];
-        this.district = info[3];
-        this.population = Integer.parseInt(info[4]);
-        if (info.length > 5) {
-            this.foundation = info[5];
-        } else {
-            this.foundation = "";
-        }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public int getPopulation() {
-        return population;
-    }
-
-    public String getFoundation() {
-        return foundation;
-    }
-
-    @Override
-    public String toString() {
-        return "City{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", region='" + region + '\'' +
-                ", district='" + district + '\'' +
-                ", population=" + population +
-                ", foundation='" + foundation + '\'' +
-                '}';
     }
 }
