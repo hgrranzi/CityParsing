27f65f8badcba455e7576319d0e5021dcfba5437
hgrranzi <vkh.mariia@gmail.com>
2021-11-22 17:13:38 +0300

    Created the CityParsing class

--- task2/Main.java
+++ task2/Main.java
@@ -1,38 +1,16 @@
 package task2;
 
-import java.util.*;
-import java.io.*;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<City> cityList;
-
         try {
-            cityList = takeInfo();
-            cityList.sort(new Comparator<City>() {
-                @Override
-                public int compare(City city1, City city2) {
-                    return city1.getName().compareToIgnoreCase(city2.getName());
-                }
-            });
-            for (City city : cityList) {
-                System.out.println(city);
-            }
+            CityParsing parser = new CityParsing();
+            parser.sortCitiesByName();
+            parser.printCities();
         } catch (FileNotFoundException e) {
             System.out.println("File error");
         }
-    }
 
-    static ArrayList<City> takeInfo() throws FileNotFoundException {
-        ArrayList<City> cityList = new ArrayList<>();
-        File file = new File("city_ru.csv");
-        Scanner scanner = new Scanner(file);
-        String[] info;
-        while (scanner.hasNextLine()) {
-            info = scanner.nextLine().split(";");
-            cityList.add(new City(info));
-        }
-        scanner.close();
-        return cityList;
     }
 }
